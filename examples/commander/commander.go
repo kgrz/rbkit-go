fff040f7bf519e7c4da067276ecdbb3db1da32d9
Kashyap <kashyap.kmbc@gmail.com>
2015-07-27 11:35:49 +0530

    Use unpack package in command test

--- examples/commander/commander.go
+++ examples/commander/commander.go
@@ -5,8 +5,8 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"time"
 
+	"github.com/kgrz/rbkit-go/unpack"
 	"github.com/ugorji/go/codec"
 	"github.com/vaughan0/go-zmq"
 )
@@ -88,91 +88,10 @@ func decodeMsgpackAndPrint(parts [][]byte) {
 			var dec *codec.Decoder = codec.NewDecoderBytes(part, &h)
 			err := dec.Decode(&msg)
 			checkError(err)
-			unpacked := unpackHandshake(msg)
-			unpacked.Print()
+			unpacked := unpack.HandshakeEvt(msg)
+			fmt.Println(&unpacked)
 		}
 	} else {
 		fmt.Println("received ", string(joinedBytes))
 	}
-}
-
-type HandshakeResponse struct {
-	EventType int64
-	Timestamp float64
-	Payload   handshakePayload
-}
-
-type handshakePayload struct {
-	ServerVersion      string
-	ProtocolVersion    string
-	ProcessName        string
-	Pwd                string
-	Pid                uint64
-	ObjectTraceEnabled bool
-}
-
-func parseReponse(parts [][]byte, h codec.MsgpackHandle, msg map[int]interface{}) {
-	joinedBytes := bytes.Join(parts, nil)
-
-	if len(joinedBytes) != 2 {
-		// This is a msgpack message
-		for _, part := range parts {
-			var dec *codec.Decoder = codec.NewDecoderBytes(part, &h)
-			err := dec.Decode(&msg)
-			checkError(err)
-			unpacked := unpackHandshake(msg)
-			unpacked.Print()
-		}
-	} else {
-		fmt.Println("\n")
-		fmt.Println("received ", string(joinedBytes))
-		fmt.Println("\n")
-	}
-
-}
-
-func unpackHandshake(payload map[int]interface{}) (response HandshakeResponse) {
-	payloadMap := payload[2].(map[interface{}]interface{})
-	var objectTraceEnabled bool
-
-	if payloadMap["object_trace_enabled"].(int64) == 0 {
-		objectTraceEnabled = false
-	} else {
-		objectTraceEnabled = true
-	}
-
-	handshakePayloadObj := handshakePayload{
-		ServerVersion:      payloadMap["rbkit_server_version"].(string),
-		ProtocolVersion:    payloadMap["rbkit_protocol_version"].(string),
-		ProcessName:        payloadMap["process_name"].(string),
-		Pwd:                payloadMap["pwd"].(string),
-		Pid:                payloadMap["pid"].(uint64),
-		ObjectTraceEnabled: objectTraceEnabled,
-	}
-
-	response = HandshakeResponse{
-		EventType: payload[0].(int64),
-		Timestamp: payload[1].(float64),
-		Payload:   handshakePayloadObj,
-	}
-
-	return
-}
-
-func (h HandshakeResponse) Print() {
-	fmt.Println("\n")
-	fmt.Println("Event Type: Handshake")
-	fmt.Println("Timestamp : ", time.Unix(int64(h.Timestamp), 0))
-	fmt.Println("Rbkit Server Version: ", h.Payload.ServerVersion)
-	fmt.Println("Rbkit Protocol Version: ", h.Payload.ProtocolVersion)
-	fmt.Println("Process Name: ", h.Payload.ProcessName)
-	fmt.Println("Working Directory: ", h.Payload.Pwd)
-	fmt.Println("Pid: ", h.Payload.Pid)
-
-	if h.Payload.ObjectTraceEnabled {
-		fmt.Println("Object Trace Enabled")
-	} else {
-		fmt.Println("Object Trace Not Enabled")
-	}
-	fmt.Println("\n")
 }
